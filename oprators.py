07d44463e785c01bac88c1e122f97b07c593e3cc
namujagtap <88942931+namujagtap@users.noreply.github.com>
2021-08-14 23:02:24 +0530

    14 aug

---
+++ oprators.py
@@ -0,0 +1,48 @@
+x=10
+y=20
+
+#arthematic oprator
+print("\n addition is=",x+y)
+print("\n substraction is=",x-y)
+print("\n dividion is=",x/y)
+print("\n multiplicatin is=",x*y)
+
+#comparison oprators
+print("x is equal to y",x==y)
+print("x not equal to y",x!=y)
+print("x greater than y",x>y)
+print("x less than y",x<y)
+print("x greater than or equal to y",x>=y)
+print("x less than or equal to y",x<=y)
+
+#logical oprator
+a = True
+b = False
+print ("a and b is ",a & b)
+print ("a or b is ",a or b)
+print (" not b is ",not b )
+#identify oprators
+S = 100
+N = 200
+print("S is N",S is N)
+print(" S is not N",S is not N)
+
+#membership oprators
+
+x = 'Hello world'
+y =  {1:'a',2:'b'}
+
+print("H in x",'H' in x)
+print("hello not in x",'hello' not in x)
+print("1 in y",1 in y)
+print("a in y",'a' in y)
+
+#bitwise oprators
+g = 60
+a = 80
+print("g & a",g & a)
+print("g | a",g | a)
+print("~ g",~a)
+print("g ^ a",g ^ a)
+print("g >> a",g >> a)
+print("g << a", g << a)
