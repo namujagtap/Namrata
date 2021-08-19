784c0276e434a233227620a13159b9e5e3cf5dea
namujagtap <88942931+namujagtap@users.noreply.github.com>
2021-08-19 12:53:59 +0530

    Add files via upload

---
+++ assgniment1.py
@@ -0,0 +1,37 @@
+# reverse a string and print it on console "python skills".
+str = "python skills" 
+print("extract all list",str) 
+print("reverse elements",str[::-1])
+
+# assign string to x variable in  DD-MM-YYYY format extract and print year from string 
+
+x=("my birth date is 22-01-2004" )
+print('extract all list',x)
+print('day of my birth is',22)
+print('month of my birth is',1)
+print('year of my birth is ',2004)
+
+#in a small comapany the avrage salary of three employee is rs 1000 per week if one employee earns rs 1100 and other earns rs 500,how much willthe third employee earn?solve by using java progra
+
+s3=3000-1100-500
+print("the salary of 3rd employee is",s3)
+
+#write program convert a percentage to a fraction (to convert a percentage into fraction let say 30% to a fraction )
+
+per = 30
+f = 30/100
+n =30
+d = 100
+numeritor = 30/10
+denomenitor = 100/10
+print(int(numeritor),"/",denomenitor)
+
+#write a program a train 340 m long is running at a speed of 45 km/hr what time will it take to cross a 160 m long tunnel?
+
+traind=340
+tunneld=160
+s=45
+speed=45000/3600
+distance=340+160
+time=distance/speed
+print(time,"sec time required to cross a tunnel is :",time,"second")
