d8a3c69457982714484f0f765bf90ad1183696eb
Namu <88942931+namujagtap@users.noreply.github.com>
2021-08-23 13:48:29 +0530

    Add files via upload

---
+++ exceptionhandling.py
@@ -0,0 +1,47 @@
+#Exception Handling
+#NameError
+ namrata = input()
+prit(namrata)
+
+#overflowerror
+print("Simple program for showing overflow error")
+print("\n")
+import math
+print("The exponential value is")
+print(math.exp(5000))
+
+#syntaxerror
+'pam': 24,
+    'jim': 24
+    'michael': 43
+}
+print(f'Michael is {ages["michael"]} years old.')ge)
+
+#IndentationError
+for i in range(1,24):
+    print(i)
+if i == 8:
+break
+
+#TypeError
+namu= "Geeks"
+num = 4
+print(namu+ num + namu)
+
+#Unbodelocalerror
+l=50
+def foo():
+    l+=1
+    print(1)
+foo()
+
+#ValueError
+
+float("namrata")
+
+#ZeroDivisionError
+
+n=5
+s=0
+c=n/s
+print(c)
