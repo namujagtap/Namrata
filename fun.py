c1415442f0fdb503d8ebfb9a5c9430b87884ba7c
Namu <88942931+namujagtap@users.noreply.github.com>
2021-08-29 15:13:56 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,10 @@
+from namrata.addition.add import add    
+from namrata.division.divifile import divi
+from namrata.multiplication.multfile import mult
+from namrata.substraction.subfile import sub
+
+
+add(3,5)
+divi(5,7)
+mult(1,9)
+sub(7,9)
