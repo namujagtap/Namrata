95c884bdaaa37a9edd154b10d2f3293f549f9f26
Namu <88942931+namujagtap@users.noreply.github.com>
2021-09-02 15:09:02 +0530

    Add files via upload

---
+++ database/studentdb.py
@@ -0,0 +1,20 @@
+#wite a program to create database studentdb
+import MySQLdb
+
+try:
+    query="CREATE DATABASE studentdb "
+    mycon=MySQLdb.connect(host="localhost",user="root",passwd="")
+    print("mycon excecute")
+    cur=mycon.cursor()
+    print("cursor excecute")
+
+    cur.execute(query)
+    print("execute query")
+    mycon.commit()
+    print(query+"table created .... ")
+except:
+    print("values inserted...")
+finally:
+    cur.close()
+    print("cursor connection close....")
+    mycon.close()
