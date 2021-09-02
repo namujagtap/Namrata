f25964999c65eb38cc16c7dc1efc090d91144fc3
Namu <88942931+namujagtap@users.noreply.github.com>
2021-09-02 15:04:37 +0530

    Add files via upload

--- pythonworkplace/database/delete.py
+++ pythonworkplace/database/delete.py
@@ -1,1 +1,22 @@
+#write program to delete specific eow of table
+import MySQLdb
 
+try:
+
+    mycon=MySQLdb.connect(host="localhost",user="root",passwd="",database="studentdb")
+    print("mycon excecute")
+    cur=mycon.cursor()
+    print("cursor excecute")
+    query="alter table stdinfo drop column address"
+    cur.execute(query)
+
+    print("execute query")
+    mycon.commit()
+    print(query+"table altered .... ")
+except:
+    print("table not altered...")
+finally:
+    cur.close()
+    print("cursor connection close...")
+    mycon.close()
+    print("DB connection close...")
